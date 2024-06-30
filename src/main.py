3875c811c3516864b4baa3a3a1a421ea386e1a65
Brett Henderson <BrettsVoid@gmail.com>
2024-06-30 22:28:40 +0300

    add text node

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, text_type_bold
+
+
+def main():
+    text_node = TextNode("This is a text node", text_type_bold, "https://www.boot.dev")
+    print(text_node)
+
+
+main()
